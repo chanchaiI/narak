71f0142c1d5415159d09cf2b5a1140c51cc6d53e
Chanchai Itthiamornlerd <chanchai.itthiamornlerd@allianz.com>
2016-02-10 17:41:41 +0700

    modify gulp

--- gulp/injector.js
+++ gulp/injector.js
@@ -1,39 +1,43 @@
 var gulp = require('gulp');
 var path = require('path');
+var wiredep = require('wiredep');
+var angularFilesort = require('gulp-angular-filesort');
+var inject = require('gulp-inject');
 
 
 gulp.task('inject', function () {
 
     var injectOptions = {
-
+        relative: true
     };
 
     var injectStyles = gulp.src([
             // selects all css files from the .tmp dir
-            paths.tmp + '/**/*.css'
+            path.tmp + '/**/*.css'
         ], { read: false }
     );
 
     var injectScripts = gulp.src([
         // selects all js files from .tmp dir
-        paths.tmp + '/**/*.js',
+        path.tmp + '/**/*.js',
         // but ignores test files
-        '!' + paths.src + '/**/*.test.js'
+        '!' + path.src + '/**/*.test.js'
         // then uses the gulp-angular-filesort plugin
         // to order the file injection
-    ]).pipe($.angularFilesort()
-        .on('error', $.util.log));
-    // tell wiredep where your bower_components are
+    ]).pipe(angularFilesort());
+        //.on('error', $.util.log));
+
+
     var wiredepOptions = {
         directory: 'bower_components'
     };
 
-    return gulp.src(paths.src + '/*.html')
-        .pipe($.inject(injectStyles, injectOptions))
-        .pipe($.inject(injectScripts, injectOptions))
+    return gulp.src(path.src + '/*.html')
+        .pipe(inject(injectStyles, injectOptions))
+        .pipe(inject(injectScripts, injectOptions))
         .pipe(wiredep(wiredepOptions))
         // write the injections to the .tmp/index.html file
-        .pipe(gulp.dest(paths.tmp));
+        .pipe(gulp.dest(path.tmp));
     // so that src/index.html file isn't modified  
     // with every commit by automatic injects
 
